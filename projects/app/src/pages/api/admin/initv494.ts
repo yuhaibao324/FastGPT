bf7249968b2a2c5be1fdbf080ec4919fdd27bd29
archer <545436317@qq.com>
2025-04-02 11:29:01 +0800

    update init sh

--- projects/app/src/pages/api/admin/initv494.ts
+++ projects/app/src/pages/api/admin/initv494.ts
@@ -8,12 +8,11 @@ import {
   TeamAppCreatePermissionVal,
   TeamDatasetCreatePermissionVal
 } from '@fastgpt/global/support/permission/user/constant';
+import { retryFn } from '@fastgpt/global/common/system/utils';
 
-async function handler(req: NextApiRequest, _res: NextApiResponse) {
-  await authCert({ req, authRoot: true });
-  // 更新团队权限：
-  // 目前所有有 TeamWritePermission 的，都需要添加三个新的权限。
-
+// 更新团队权限：
+// 目前所有有 TeamWritePermission 的，都需要添加三个新的权限。
+const updateTeamPermission = async () => {
   const rps = await MongoResourcePermission.find({
     resourceType: 'team',
     teamId: { $exists: true },
@@ -23,15 +22,24 @@ async function handler(req: NextApiRequest, _res: NextApiResponse) {
   for (const rp of rps) {
     const per = new TeamPermission({ per: rp.permission });
     if (per.hasWritePer) {
-      const newPer = per.addPer(
-        TeamAppCreatePermissionVal,
-        TeamDatasetCreatePermissionVal,
-        TeamApikeyCreatePermissionVal
-      );
-      rp.permission = newPer.value;
-      rp.save();
+      try {
+        const newPer = per.addPer(
+          TeamAppCreatePermissionVal,
+          TeamDatasetCreatePermissionVal,
+          TeamApikeyCreatePermissionVal
+        );
+        rp.permission = newPer.value;
+        await retryFn(async () => await rp.save());
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
+};
+async function handler(req: NextApiRequest, _res: NextApiResponse) {
+  await authCert({ req, authRoot: true });
+
+  await updateTeamPermission();
 
   return { success: true };
 }
